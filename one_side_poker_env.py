b6dff5640183b80240bdd74949c56b7c3e47fe02
shingaryu <shingaryu@gmail.com>
2020-12-28 01:18:00 +0100

    Refactor

---
+++ one_side_poker_env.py
@@ -0,0 +1,68 @@
+from pokertrees import *
+import random
+from poker_env import PokerEnv
+
+class OneSidePokerEnv(PokerEnv):
+    def __init__(self, rules, player):
+        PokerEnv.__init__(self, rules)
+        self.player = player
+        self.opp_strategies = [ {} for _ in range(self.rules.players)]
+
+    def reset(self):
+        self.cfr()
+        self.root = self.tree.root.children[0]
+        self.root = self.skip_opp_node(self.root)
+        if (type(self.root) is TerminalNode):
+          raise Exception('Terminal node exists at the top of game tree. Something is wrong.')
+          
+        infoset, valid_actions = self.node_observation(self.root)
+        return self.root.player, infoset, valid_actions, 0, False
+
+    def step(self, action):
+        self.root = self.root.get_child(action)
+        self.root = self.skip_opp_node(self.root)
+        if type(self.root) is TerminalNode:
+            # return self.cfr_terminal_node(root, reachprobs, sampleprobs)
+            # set terminal utility
+            payoffs = [0 for _ in range(self.rules.players)]
+            for hands,winnings in self.root.payoffs.items():
+                if not self.terminal_match(hands):
+                    continue
+                for player in range(self.rules.players):
+                    # prob = 1.0
+                    # for opp,hc in enumerate(hands):
+                    #     if opp != player:
+                    #         prob *= reachprobs[opp]
+                    # payoffs[player] = prob * winnings[player] / sampleprobs
+                    payoffs[player] = winnings[player]
+
+            return (None, None, None, payoffs, True)
+
+        infoset, valid_actions = self.node_observation(self.root)
+        return self.root.player, infoset, valid_actions, 0, False
+
+    def skip_opp_node(self, root):
+        while True:
+            if type(root) is HolecardChanceNode:
+                assert(len(root.children) == 1)
+                root = root.children[0]
+                continue
+            if type(root) is BoardcardChanceNode:
+                root = random.choice(root.children)
+                continue
+            if (type(root) is ActionNode and root.player is not self.player):
+                infoset, valid_actions = self.node_observation(root)
+                opp_action = None
+                if infoset not in self.opp_strategies[root.player]:
+                  opp_action = random.choice(valid_actions)
+                else:
+                  opp_action = self.opp_strategies[root.player][infoset]
+                root = root.get_child(opp_action)
+                continue
+            else:
+                break
+          
+        return root
+      
+    def set_opp_strategies(self, strategies):
+      self.opp_strategies = strategies
--- tests/test_batch.py
+++ tests/test_batch.py
@@ -4,7 +4,8 @@ sys.path.insert(0,os.path.realpath('.'))
 from pokerstrategy import *
 from pokergames import *
 from pokercfr import *
-from poker_env import PokerEnv, OneSidePokerEnv
+from poker_env import PokerEnv
+from one_side_poker_env import OneSidePokerEnv
 from env_oscfr import *
 from env_oscfr_with_sp import *
 
@@ -21,17 +22,16 @@ print ''
 print 'Computing NE for Kuhn poker'
 kuhn = kuhn_rules()
 
-env = PokerEnv(kuhn)
+env_both = PokerEnv(kuhn)
 env_player = OneSidePokerEnv(kuhn, 0)
 env_opp = OneSidePokerEnv(kuhn, 1)
-# cfr = EnvOSCFRWithSP(env)
-cfr = EnvOSCFRWithSP(env.rules)
+cfr = EnvOSCFRWithSP(kuhn)
 
 iterations_per_block = 10000
 blocks = 100
 for block in range(blocks):
     print 'Iterations: {0}'.format(block * iterations_per_block)
-    # cfr.run(iterations_per_block)
+    # cfr.run(env_both, iterations_per_block)
     for _env in [env_player, env_opp]:
         cfr.run(_env, iterations_per_block)
         strt = cfr.fixed_strategy()
